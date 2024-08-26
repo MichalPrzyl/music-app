315a648673e86f73433833f8fb3265f908eae243
Michał Przyłucki <michal.przyl@gmail.com>
2024-08-26 19:15:14 +0200

    Add raylib to project

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -o program main.c -Wall
+gcc -o program main.c -I./ext_src/raylib/src -L./ext_src/raylib/build/raylib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+#include "raylib.h"
 #include <stdio.h>
 
-int main(){
-  printf("hello world\n");
-  return 0;
+int main(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+  printf("Hello world\n");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
