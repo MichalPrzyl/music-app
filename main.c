07a292a8794a3b5158b642f543d8cea40275d97f
Michał Przyłucki <michal.przyl@gmail.com>
2024-08-26 19:22:04 +0200

    Add: Play music

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include "raylib.h"
-#include <stdio.h>
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(800, 600, "Audio Example");
+    InitAudioDevice();
 
-  printf("Hello world\n");
+    Sound sound = LoadSound("music/last_resort.mp3");
+
+    PlaySound(sound);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(RAYWHITE);
+        DrawText("Playing audio... Press ESC to exit", 190, 280, 20, LIGHTGRAY);
         EndDrawing();
     }
 
+    UnloadSound(sound);
+    CloseAudioDevice();
     CloseWindow();
 
     return 0;
 }
+
