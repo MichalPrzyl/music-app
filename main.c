3cc4fdde1e20b0d8332f5270dc8852f2a4cfa6ed
Michał Przyłucki <michal.przyl@gmail.com>
2024-08-28 19:19:20 +0200

    Change colors

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
+
 #include "raylib.h"
 #include <math.h>
 
 #define NUM_BANDS 16
-#define AMPLITUDE 300
+#define AMPLITUDE 100
 #define SPEED 2.0f
 
 int main(void)
 {
-    InitWindow(800, 600, "equalizzz");
-
+    InitWindow(800, 600, "Equalizer Simulation");
     InitAudioDevice();
 
     Sound sound = LoadSound("music/last_resort.mp3");
@@ -23,19 +23,22 @@ int main(void)
         // update time
         time += GetFrameTime();
 
+        // Simulate different frequencies by modulating the amplitude
         for (int i = 0; i < NUM_BANDS; i++)
         {
-            bands[i] = fabs(sinf((1.0f * PI * i / NUM_BANDS) + (time * SPEED))) * AMPLITUDE;
+            float frequency = (i + 1) * 0.1f;  // Simulate different frequencies
+            bands[i] = fabs(sinf((frequency * PI) + (time * SPEED))) * AMPLITUDE;
         }
 
         BeginDrawing();
         ClearBackground(BLACK);
 
+        // Draw bars representing the simulated frequencies
         for (int i = 0; i < NUM_BANDS; i++)
         {
             float x = (800 / NUM_BANDS) * i;
             float height = bands[i];
-            DrawRectangle(x, 600 - height, (800 / NUM_BANDS) - 2, height, height > (AMPLITUDE/2) ? RED : GREEN);
+            DrawRectangle(x, 600 - height, (800 / NUM_BANDS) - 2, height, i % 2 == 0 ? RED : GREEN);
         }
 
         DrawText("Playing audio... Press ESC to exit", 190, 550, 20, LIGHTGRAY);
